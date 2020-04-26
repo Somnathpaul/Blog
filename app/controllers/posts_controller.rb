4e08ff5717510a4a01799a9abb25e9ffd69903d4
Somnath Paul <Somnath4x@gmail.com>
2020-04-26 12:28:19 +0530

    Add new route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,23 @@ class PostsController < ApplicationController
     end
 
     def new
-        
+        @post = Post.new
     end
 
     def create
+        @post = Post.new(post_params)
+
+        if @post.save
+            redirect_to @post
+        else
+            ender new
+        end
         
+    end
+
+    private 
+
+    def post_params
+        params.require(:post).permit(:title, :content)
     end
 end
