ee25c881481ad83bdb748540484cac0fa58b6078
Somnath Paul <Somnath4x@gmail.com>
2020-05-21 21:36:49 +0530

    comments section added

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_18_151900) do
+ActiveRecord::Schema.define(version: 2020_05_19_151042) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
